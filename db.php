fad3dfcb8cef2f7f1eb4a7a1640bbba6c78445c5
Justin <ju3tin@hotmail.co.uk>
2018-12-17 16:36:37 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("eu-cdbr-west-02.cleardb.net:3306","befa7dfea41859","bd06c2aa","register");
+$con = mysqli_connect("eu-cdbr-west-02.cleardb.net:3306","befa7dfea41859","bd06c2aa","heroku_5761656d599b4cc");
 // Check connection
 if (mysqli_connect_errno())
   {
