f7590f631d34b8ef715c25cb778d62d7c7949832
Justin <ju3tin@hotmail.co.uk>
2018-12-17 16:33:28 +0000

    Update index.php

--- index.php
+++ index.php
@@ -190,6 +190,43 @@
         <h2 class="h3 text-gray">We are Transparent, Reliable, and keep it simple!</h2>
         <div class="form-cover">
           <div class="form">
+            
+            
+            <?php
+require('db.php');
+// If form submitted, insert values into the database.
+if (isset($_REQUEST['username'])){
+        // removes backslashes
+	$username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+	$username = mysqli_real_escape_string($con,$username); 
+	$email = stripslashes($_REQUEST['email']);
+	$email = mysqli_real_escape_string($con,$email);
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+	$trn_date = date("Y-m-d H:i:s");
+        $query = "INSERT into `users` (username, password, email, trn_date)
+VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+        $result = mysqli_query($con,$query);
+        if($result){
+            echo "<div class='form'>
+<h3>You are registered successfully.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+        }
+    }else{
+?>
+<div class="form">
+<h1>Registration</h1>
+<form name="registration" action="" method="post">
+<input type="text" name="username" placeholder="Username" required />
+<input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password" placeholder="Password" required />
+<input type="submit" name="submit" value="Register" />
+</form>
+</div>
+<?php } ?>
+            
+            
             <form id="bottom_form" data-toggle="validator" class="shake">
               <div class="form-group half">
                 <input class="form-control" type="text" id="firstname2" placeholder="First name" required="required" />
